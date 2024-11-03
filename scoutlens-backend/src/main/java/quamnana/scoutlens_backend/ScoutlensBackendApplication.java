1fbd4089a318c7f756201400e1825f097ac76c2e
Nana Quam <quamgyambrah@gmail.com>
2024-11-02 23:25:25 -0400

    [INITIAL] initial commit

---
+++ scoutlens-backend/src/main/java/quamnana/scoutlens_backend/ScoutlensBackendApplication.java
@@ -0,0 +1,13 @@
+package quamnana.scoutlens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutlensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutlensBackendApplication.class, args);
+	}
+
+}
---
+++ scoutlens-backend/src/test/java/quamnana/scoutlens_backend/ScoutlensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package quamnana.scoutlens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutlensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
